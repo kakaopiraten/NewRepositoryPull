6f89e1c1f4a1ada5177de1e4c1a7b88ad458b6bc
kakaopiraten <55191176+kakaopiraten@users.noreply.github.com>
2022-01-03 09:09:06 +0100

    Initialize

---
+++ Collaboration/Collaboration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Collaboration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
